9ca09e3dae1f6c877e1e26c8b0bb600403f698b3
= <debowale01@gmail.com>
2017-07-18 07:19:48 +0100

    pushed repo to the server

--- js/script.js
+++ js/script.js
@@ -44,22 +44,22 @@ $(document).ready(function(){
   });
 
 
-  //Check to see if the window is top if not then display button
-  $(window).scroll(function(){
-    if ($(this).scrollTop() > 100) {
-      $('.top').fadeIn();
-    } else {
-      $('.top').fadeOut();
-    }
-  });
-  
-  //Click event to scroll to top
-  $('.top').click(function(){
-    $('html, body').animate({scrollTop : 0},800);
-    return false;
-  });
+    //Check to see if the window is top if not then display button
+    $(window).scroll(function(){
+      if ($(this).scrollTop() > 100) {
+        $('.top').fadeIn();
+      } else {
+        $('.top').fadeOut();
+      }
+    });
+    
+    //Click event to scroll to top
+    $('.top').click(function(){
+      $('html, body').animate({scrollTop : 0},800);
+      return false;
+    });
 
-});
+  });
 
 
 //to slide slowly to the named anchor
